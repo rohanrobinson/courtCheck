135c751afbaeed4dd2121ac890ac252ab8b5048d
rohanrobinson <35516929+rohanrobinson@users.noreply.github.com>
2019-10-14 15:42:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ var date =  new Date();
 var today = (date.getMonth() + 1) + '-' + (date.getDate());
 document.getElementById("currentDate").innerHTML = today;
 
-court2Info=['Reserved 4:30 to 10:30 for Club Basketball practice', 'Reserved 3:45 to 9:00  for Club Basketball practice', 
-           'Available all day for shooting around/pickup'];
+court2Info=['Reserved 3:45 to 9:00', 'Reserved 4:30 to 10:30', 
+           'Reserved 3:45 to 9:00'];
 
 function getReservation(today, court2Info) {
   courtName = "Court 2: ";
-  if (today.slice(3) == 1) {
+  if (today.slice(3) == 14) {
     return courtName + court2Info[0];
   } 
   
-  if (today.slice(3) == 2) {
+  if (today.slice(3) == 15) {
     return courtName + court2Info[1];
   }
   
-  if (today.slice(3) == 3) {
+  if (today.slice(3) == 16) {
     return courtName + court2Info[2]
       }
 }
