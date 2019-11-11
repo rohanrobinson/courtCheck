8b94678188582bec21eabf2550e30206e27a671a
rohanrobinson <35516929+rohanrobinson@users.noreply.github.com>
2019-11-11 14:23:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ var date =  new Date();
 var today = (date.getMonth() + 1) + '-' + (date.getDate());
 document.getElementById("currentDate").innerHTML = today;
 
-court2Info = ['Available for shootaround all day', 'Reserved from 3:45PM to 6:45PM', 
-           'Reserved from 4:30PM to 10:30PM', 'Reserved from 3:45PM to 9:00PM'];
+court2Info = [ 'Reserved from 3:45PM to 9:00PM', 
+           'Reserved from 4:30PM to 10:30PM', 'Reserved from 3:45PM to 6:45PM', 'Reserved from 3:45PM to 9:00PM'];
 
 function getReservation(today, court2Info) {
   courtName = "Court 2: ";
            
-  if (today.slice(3) == 3) {
+  if (today.slice(3) == 11) {
     return courtName + court2Info[0];
   }       
-  if (today.slice(3) == 4) {
+  if (today.slice(3) == 12) {
     return courtName + court2Info[1];
   } 
   
-  if (today.slice(3) == 5) {
+  if (today.slice(3) == 13) {
     return courtName + court2Info[2];
   }
   
-  if (today.slice(3) == 6) {
+  if (today.slice(3) == 14) {
     return courtName + court2Info[3]
       }
            
